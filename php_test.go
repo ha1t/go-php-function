c0af94cab2a057a359ec9e82898f3740b858b8fd
halt <haltfeits_at_gmail.com>
2016-01-15 20:30:43 +0900

    add example

--- php_test.go
+++ php_test.go
@@ -1,9 +1,15 @@
 package php_test
 
 import (
+	"fmt"
 	"github.com/ha1t/go-php-function"
 	"testing"
 )
+
+func ExampleBasename() {
+	fmt.Println(php.Basename("/usr/bin/hogehoge.php"))
+	// Output: hogehoge.php
+}
 
 func TestBasename(t *testing.T) {
 	if i := php.Basename("/hoge/huga/base.php"); i != "base.php" {
