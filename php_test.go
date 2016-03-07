31782eaea4de65cf38e104b25bd2405633d45037
halt <haltfeits_at_gmail.com>
2016-03-07 20:03:54 +0900

    add InArray

--- php_test.go
+++ php_test.go
@@ -18,3 +18,25 @@ func TestBasename(t *testing.T) {
 		t.Log("ok")
 	}
 }
+
+func TestInArray(t *testing.T) {
+	itemList := []string{"hoge", "huga", "moge"}
+
+	if php.InArray("hoge", itemList) {
+		t.Log("ok")
+	} else {
+		t.Error("failed")
+	}
+
+	if php.InArray("not_found", itemList) {
+		t.Error("failed")
+	} else {
+		t.Log("ok")
+	}
+
+	if php.InArray("", itemList) {
+		t.Error("failed")
+	} else {
+		t.Log("ok")
+	}
+}
